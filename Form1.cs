781e618148a1ab8db8c69570df120e20fafeaa4b
bbbbbbil-gates <1123764877@qq.com>
2020-06-07 16:28:13 +0800

    connect db

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,29 @@ namespace hot_summer
 {
     public partial class Form1 : Form
     {
+        private MySqlCommand cmd = null;
+        private MySqlConnection conn = null;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String connstring = "Server=localhost;Database =test_schema;Uid=root;Pwd=ca.0123;";
+
+            conn = new MySqlConnection(connstring);
+
+            conn.Open();
+
+            string insert = "INSERT INTO `test_schema`.`test_table` (`idtest_table`, `test_tablecol`, `test_tablecol1`) VALUES ('4', 'zxc', '100');";
+
+            cmd = new MySqlCommand(insert, conn); 
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();  
         }
     }
 }
